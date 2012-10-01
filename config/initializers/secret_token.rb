190d4d5d2507758613766c2f534ebc9ff9442c47
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-10-01 22:09:50 +0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerpodcasts::Application.config.secret_token = '696bb0c6914e882b8dc450e133732f63f85a262d5c94fed196bba7495e8d47e400b56fdc2d00ac867288e8c39508cee2969f88a22f620c7c7cc86fee6d43745b'
