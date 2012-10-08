39dd6e48d9caee72735ec2a248dfa0b7ee135074
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-10-08 22:22:05 +0300

    Add NodeUp, Hanselminutes and Herding Code to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-Podcast.create([{name: 'Ruby Rogues', feed_url: 'http://feeds.feedburner.com/RubyRogues' },
-                {name: 'Javascript Jabber', feed_url: 'http://feeds.feedburner.com/JavascriptJabber' }])
+Podcast.create([{ name: 'Ruby Rogues', feed_url: 'http://feeds.feedburner.com/RubyRogues' },
+                { name: 'Javascript Jabber', feed_url: 'http://feeds.feedburner.com/JavascriptJabber' },
+                { name: 'NodeUp', feed_url: 'http://feeds.feedburner.com/NodeUp' },
+                { name: 'Herding Code', feed_url: 'http://feeds.feedburner.com/herdingcode' },
+                { name: 'Hanselminutes', feed_url: 'http://feeds.feedburner.com/Hanselminutes' }])
